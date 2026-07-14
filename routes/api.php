1b6039f23d4992d3282163de6441418ea29bed94
sheinafathurr1 <sheinafathur@gmail.com>
2026-07-14 14:37:25 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::prefix('v3/{env}/{uniq}')->group(function () {
 
 // 2. Endpoint API Gateway TOPSIS
 Route::post('topsis/recommendation', [TopsisController::class, 'generateSlrRecommendation']);
+Route::post('topsis/validate', [TopsisController::class, 'validateScores']);
 
 // 3. Endpoint pembanding: TOPSIS Data-to-Compute (baseline)
 Route::post('topsis/baseline-recommendation', [BaselineTopsisController::class, 'generateSlrRecommendation']);
